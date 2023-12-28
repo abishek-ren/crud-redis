7695c735c1bdbf2b445f5f3205b2c955df080819
Abishek Geddallu Rengarajan <thegrabishek@gmail.com>
2023-12-28 02:40:15 -0700

    enable caching

--- src/main/java/tech/abishek/crudredis/repository/ProductDao.java
+++ src/main/java/tech/abishek/crudredis/repository/ProductDao.java
@@ -1,6 +1,8 @@
 package tech.abishek.crudredis.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 import tech.abishek.crudredis.entity.Product;
@@ -11,15 +13,18 @@ import java.util.List;
 public class ProductDao {
 
     public static final String HASH_KEY = "PRODUCT";
+    public static final String PRODUCTS = "products";
 
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @CacheEvict(value=PRODUCTS, allEntries=true)
     public Product save(Product product) {
         redisTemplate.opsForHash().put(HASH_KEY, product.getId(), product);
         return product;
     }
 
+    @Cacheable(value = "products")
     public List<Product> findAll() {
         return redisTemplate.opsForHash().values(HASH_KEY);
     }
@@ -28,6 +33,7 @@ public class ProductDao {
         return (Product) redisTemplate.opsForHash().get(HASH_KEY, id);
     }
 
+    @CacheEvict(value=PRODUCTS, allEntries=true)
     public String deleteProduct(Long id) {
         redisTemplate.opsForHash().delete(HASH_KEY, id);
         return "Product removed !! " + id;
